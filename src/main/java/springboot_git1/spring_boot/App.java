9c30f557d08d4885611e3d38bb2445c41af44b53
Administrator <1103694769@qq.com>
2017-08-15 22:32:00 +0800

    feiya

---
+++ src/main/java/springboot_git1/spring_boot/App.java
@@ -0,0 +1,13 @@
+package springboot_git1.spring_boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
